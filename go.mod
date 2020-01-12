19944b1f36195379698a279acc04c8cbbca91897
t1una <totugekiluna@gmail.com>
2020-01-12 22:48:53 +0900

    [update] sqlite3へ

--- go.mod
+++ go.mod
@@ -6,4 +6,6 @@ require (
 	github.com/bwmarrin/discordgo v0.20.2
 	github.com/jinzhu/gorm v1.9.11
 	github.com/joho/godotenv v1.3.0
+	github.com/mattn/go-sqlite3 v2.0.2+incompatible
+	golang.org/x/tools v0.0.0-20191230220329-2aa90c603ae3 // indirect
 )
--- lib/db/db.go
+++ lib/db/db.go
@@ -2,26 +2,25 @@ package db
 
 import (
 	"discord-bot/lib/dotenv"
-	"fmt"
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"os"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func ConnectDb() *gorm.DB {
 	dotenv.EnvLoad()
-	var (
-		dbHostName = os.Getenv("POSTGRES_HOST")
-		dbPort     = os.Getenv("POSTGRES_PORT")
-		dbName     = os.Getenv("POSTGRES_DB")
-		dbUser     = os.Getenv("POSTGRES_USER")
-		dbPassword = os.Getenv("POSTGRES_PASSWORD")
-	)
-	var dbAccessString = "host=" + dbHostName + " port=" + dbPort +
-		" user=" + dbUser + " dbname=" + dbName + " password=" + dbPassword + " sslmode=disable"
-	fmt.Printf("%20s", dbAccessString)
-	db, err := gorm.Open("postgres", dbAccessString)
+	// var (
+	// 	dbHostName = os.Getenv("POSTGRES_HOST")
+	// 	dbPort     = os.Getenv("POSTGRES_PORT")
+	// 	dbName     = os.Getenv("POSTGRES_DB")
+	// 	dbUser     = os.Getenv("POSTGRES_USER")
+	// 	dbPassword = os.Getenv("POSTGRES_PASSWORD")
+	// )
+	// var dbAccessString = "host=" + dbHostName + " port=" + dbPort +
+	// 	" user=" + dbUser + " dbname=" + dbName + " password=" + dbPassword + " sslmode=disable"
+	// fmt.Printf("%20s", dbAccessString)
+	// db, err := gorm.Open("postgres", dbAccessString)
 
+	db, err := gorm.Open("sqlite3", "bot.db")
 	if err != nil {
 		panic("データベースへの接続に失敗しました")
 	}
