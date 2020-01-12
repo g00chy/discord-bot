21f46fb2dff1218efe26e2f3292c853f40c4c78a
t1una <totugekiluna@gmail.com>
2020-01-13 04:15:47 +0900

    [update] ビルドする対象が一つに

--- .vscode/build.ps1
+++ .vscode/build.ps1
@@ -1,6 +1,4 @@
 $env:GOOS = "linux"
 $env:GOARCH = "arm"
 go env GOOS GOARCH
-go build -o .\binary\afk-bot .\afk-bot\afk-bot.go
-go build -o .\binary\claim-bot .\claim-bot\claim-bot.go
-go build -o .\binary\nleave-ban-bot .\nleave-ban-bot\nleave-ban-bot.go
+go build -o .\binary\discord-bot .\main.go
