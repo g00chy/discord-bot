8283d9deaec7e97f9e0c74acaea4727f4be2900a
t1una <totugekiluna@gmail.com>
2020-01-31 23:38:34 +0900

    [add]まずは適当なコンポーネントを追加するまで

---
+++ yarn/src/ComponentTest.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class CommentList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      comments: [
+        {
+          id: 1,
+          comment: 'comment 1'
+        },
+        {
+          id: 2,
+          comment: 'comment 2'
+        },
+        {
+          id: 3,
+          comment: 'comment 3'
+        },
+        {
+          id: 4,
+          comment: 'comment 4'
+        }
+      ],
+      hasError: false,
+      isLoading: false
+    }
+  }
+  render() {
+    if (this.state.hasError) {
+      return <p>error</p>;
+    }
+    if (this.state.isLoading) {
+      return <p>loading . . . </p>;
+    }
+    return (
+      <ul>
+        {this.state.comments.map((item) => (
+          <li key={item.id}>
+            {item.comment}
+          </li>
+        ))}
+      </ul>
+    )
+  }
+}
+
+ReactDOM.render(
+  <CommentList />,
+  document.getElementById('app')
+)
