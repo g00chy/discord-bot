82303f2eb856a63a5aa9245871554d6eb328d00f
t1una <totugekiluna@gmail.com>
2020-02-01 23:18:53 +0900

    [update] ルート以外でのビルドは難しいっぽいので、environmentに値を渡してビルドするように修正

--- bot/build/make_arm.sh
+++ bot/build/make_arm.sh
@@ -2,3 +2,4 @@
 go mod download
 CC=arm-linux-gnueabi-gcc CGO_ENABLED=1 GOOS=linux GOARCH=arm go build -o ./binary/arm-discord-bot ./bots/bot.go
 CC=arm-linux-gnueabi-gcc CGO_ENABLED=1 GOOS=linux GOARCH=arm go build -o ./binary/arm-discord-web ./web/web.go
+chown ${uid}:${gid} -R ./binary
