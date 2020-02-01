3ef937a797b31b2f44281b8b3749eb14d2a21d2c
t1una <totugekiluna@gmail.com>
2020-02-01 23:08:56 +0900

    [update] ディレクトリの場所を変更

--- bot/build/docker-make-binary.sh
+++ bot/build/docker-make-binary.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+export UID=${UID}
+export GID=${GID}
 docker-compose run --rm build_arm
 docker-compose run --rm build_arm64
 docker-compose run --rm build_amd64
--- build/build.ps1
+++
@@ -1,6 +0,0 @@
-docker-compose run --rm build_arm
-echo "builded arm-----------------"
-docker-compose run --rm build_arm64
-echo "builded arm64-----------------"
-docker-compose run --rm build_amd64
-echo "builded amd64-----------------"
