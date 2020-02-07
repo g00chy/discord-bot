86d8983946705b8e1ed523a76222daf5f50702d7
t1una <totugekiluna@gmail.com>
2020-02-07 17:28:33 +0900

    [update] vendorしちゃうとvendorもrootになっちゃうので

--- bot/build/make_amd64.sh
+++ bot/build/make_amd64.sh
@@ -3,3 +3,4 @@ go mod vendor
 CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o ./binary/amd-discord-bot ./bots/bot.go
 CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o ./binary/amd-discord-web ./web/web.go
 chown ${uid}:${gid} -R ./binary
+chown ${uid}:${gid} -R ./vendor
--- bot/build/make_arm64.sh
+++ bot/build/make_arm64.sh
@@ -3,3 +3,4 @@ go mod vendor
 CC=aarch64-linux-gnu-gcc CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build -o ./binary/arm64-discord-bot ./bots/bot.go
 CC=aarch64-linux-gnu-gcc CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build -o ./binary/arm64-discord-web ./web/web.go
 chown ${uid}:${gid} -R ./binary
+chown ${uid}:${gid} -R ./vendor
