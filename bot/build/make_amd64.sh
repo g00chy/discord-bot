5f22185bbaf293ded3966191dfd902112c2bd9a6
t1una <totugekiluna@gmail.com>
2020-02-11 18:50:46 +0900

    [update] remove vendor

--- bot/build/make_amd64.sh
+++ bot/build/make_amd64.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-go mod vendor
 CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o ./binary/amd-discord-bot ./bots/bot.go
 CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o ./binary/amd-discord-web ./web/web.go
 chown ${uid}:${gid} -R ./binary
--- bot/build/make_arm64.sh
+++ bot/build/make_arm64.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-go mod vendor
 CC=aarch64-linux-gnu-gcc CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build -o ./binary/arm64-discord-bot ./bots/bot.go
 CC=aarch64-linux-gnu-gcc CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build -o ./binary/arm64-discord-web ./web/web.go
 chown ${uid}:${gid} -R ./binary
