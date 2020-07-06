59b9154de0532bcf2f70a981a78ce4e7ababd970
g00chy <totugekiluna@gmail.com>
2020-07-07 03:42:51 +0900

    add: envfile change: build's docker-compose read envfile makefile params error handling

--- bot/build/make.sh
+++ bot/build/make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-go build -o ./binary/${GOARCH}-discord-bot ./bots/bot.go
-go build -o ./binary/${GOARCH}-discord-web ./web/web.go
+go build -o ./binary/${GOARCH}-discord-bot --ldflags '-linkmode external -extldflags "-static"' ./bots/bot.go
+go build -o ./binary/${GOARCH}-discord-web --ldflags '-linkmode external -extldflags "-static"' ./web/web.go
 chown ${uid}:${gid} -R ./binary
