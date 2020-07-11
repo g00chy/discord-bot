78601a6bb289c7b7e517546785f86e6378ac1468
g00chy <totugekiluna@gmail.com>
2020-07-12 01:58:11 +0900

    change messege

--- bot/build/build.ps1
+++ bot/build/build.ps1
@@ -1,6 +1,6 @@
-docker-compose -f build-docker-compose.yml run --rm build_arm
 echo "builded arm-----------------"
-docker-compose -f build-docker-compose.yml run --rm build_arm64
+docker-compose -f build-docker-compose.yml run --rm build_arm
 echo "builded arm64-----------------"
-docker-compose -f build-docker-compose.yml run --rm build_amd64
+docker-compose -f build-docker-compose.yml run --rm build_arm64
 echo "builded amd64-----------------"
+docker-compose -f build-docker-compose.yml run --rm build_amd64
